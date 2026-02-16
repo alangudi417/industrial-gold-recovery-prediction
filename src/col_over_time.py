3c75e3e4f03c9c845afc695157e3db9d67305686
alangudi417 <211910311+alangudi417@users.noreply.github.com>
2026-02-15 19:01:21 -0600

    adding function

---
+++ src/col_over_time.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+def missing_values_over_time(df, date_column, perc, threshold=30):
+    """
+    This function is resampling the DataFrame over different frequencies, and returns 
+    the result where the average exceeds the proposed threshold
+
+    Parameters:
+        df: input
+        date_column: name of the column
+        freq: here is the resample frequency ('h', 'd', 'w', 'm')
+        threshold: minimum value to accept
+    """
+
+    # Set the frequencies: 
+    frequencies = {
+        'Hourly':'h',
+        'Daily':'d',
+        'Weekly':'w',
+        'Monthly':'m'
+    }
+
+    results = {}
+
+    for label, freq in frequencies.items():
+        result = (
+            df.set_index(date_column)
+                .resample(freq)[perc]
+                .mean()
+                .round(2)
+        )
+
+    result = result.loc[result > threshold].sort_values(ascending=False)
+    
+    if not result.empty:
+        results[label] = result
+
+    return results
